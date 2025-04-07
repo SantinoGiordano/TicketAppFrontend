cf8a6cd819068ba6d9632e67fff739acece325e3
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-07 07:12:08 -0400

    started image banner

---
+++ app/components/ImageBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const ImageBanner = () => {
+  return (
+    <>
+    <Image src={'/'} alt={''}/>
+    </>
+  )
+}
+
+export default ImageBanner
