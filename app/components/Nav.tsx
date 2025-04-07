4700bc6f722747940d893a13237f023c4dc47804
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-07 12:37:12 -0400

    inverted colors

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow-md px-4 py-3 flex justify-between items-center">
-      <div className="text-xl font-bold text-blue-600">Event</div>
-      <ul className="flex space-x-6 text-sm font-medium text-gray-700">
+    <nav className="z-100 bg-blue-900 h-20 p-5 px-4 py-3 flex justify-between items-center">
+      <div className="text-xl font-bold text-white">Event</div>
+      <ul className=" flex space-x-6 text-md font-medium text-white">
         <li>
           <Link href="/" className="hover:text-blue-500 transition-colors">
             Home
