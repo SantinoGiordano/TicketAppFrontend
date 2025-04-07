58afa48bc8a615facb97cf6b120c94f7f2dcb13a
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-07 12:37:25 -0400

    added dropdown menus

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
-
+"use client";
 import ImageBanner from "../components/ImageBanner";
 import Nav from "../components/Nav";
+import DropdownInput from "../components/ToAndFrom";
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <ImageBanner/>
+      <div className=" p-10 flex flex-col sm:flex-row items-center gap-4  bg-base-100 rounded-xl w-full">
+        <DropdownInput />
+        <DropdownInput />
+      </div>
 
+      <div className="bg-blue-100">
+        <ImageBanner />
+        <div className="divider divider-info">Learn more</div>
+      </div>
     </>
   );
 };
