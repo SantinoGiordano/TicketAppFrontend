f0e31cc6690d46c6e8c84f728712a2f098f16387
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-08 16:07:39 -0400

    added departure date

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CarouselTop from "../components/CarouselTop";
+import DepartureDate from "../components/DepartureDate";
 import ImageBanner from "../components/ImageBanner";
 import Nav from "../components/Nav";
 import DropdownInput from "../components/ToAndFrom";
@@ -9,8 +10,6 @@ const Home = () => {
     <>
       <Nav />
       <CarouselTop />
-
-      {/* Travel Form Section */}
       <div className="bg-gray-100 py-10 px-4">
         <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-6 flex flex-col sm:flex-row sm:items-center gap-6">
           <div className="w-full sm:w-1/2">
@@ -20,10 +19,9 @@ const Home = () => {
           <div className="w-full sm:w-1/2">
             <DropdownInput label="To Station" />
           </div>
+          <DepartureDate/>
         </div>
       </div>
-
-      {/* Image Banner Section */}
       <div className="bg-blue-100 py-10 px-4">
         <ImageBanner />
       </div>
