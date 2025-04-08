c404316babf9d76cbb026ab7a4c29b0db763a4c7
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-08 16:53:42 -0400

    added Daisy Calander

--- app/components/DepartureDate.tsx
+++ app/components/DepartureDate.tsx
@@ -38,18 +38,19 @@ const DepartureDate = () => {
 
   return (
     <div className="relative">
-      <button
-        popoverTarget="cally-popover1"
-        id="cally1"
-        className="px-6 py-2 text-sm font-medium border border-gray-400 rounded-lg bg-white hover:bg-gray-100 transition"
-      >
-        {date}
-      </button>
+<button
+  popoverTarget="cally-popover1"
+  id="cally1"
+  className=" sm:w-64 px-6 py-2 text-sm font-medium border border-gray-400 rounded-lg hover:bg-gray-100 transition"
+>
+  {date}
+</button>
+
 
       <div
         popover="auto"
         id="cally-popover1"
-        className="dropdown z-50 mt-2 bg-white p-4 rounded-xl shadow-xl border border-gray-200"
+        className="dropdown mt-2 bg-white p-4 rounded-xl shadow-xl border border-gray-200"
       >
         <calendar-date class="cally" ref={calendarRef}>
           <svg
