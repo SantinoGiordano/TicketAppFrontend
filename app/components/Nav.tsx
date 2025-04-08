02daa750c3b94d9c8a3a6f73208d62c54c18da34
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-07 20:50:53 -0400

    made nav fixed

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="z-100 bg-blue-900 h-20 p-5 px-4 py-3 flex justify-between items-center">
+    <nav className=" w-screen fixed z-100 bg-blue-900 h-30 p-5 px-4 py-3 flex justify-between items-center">
       <div className="text-xl font-bold text-white">Event</div>
       <ul className=" flex space-x-6 text-md font-medium text-white">
         <li>
