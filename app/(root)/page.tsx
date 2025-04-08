9d75cb29d51f667d81a3e256e804351c37dc03ec
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-08 15:25:22 -0400

    changed home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import CarouselTop from "../components/CarouselTop";
-// import DepartureDate from "../components/DepartureDate";
 import ImageBanner from "../components/ImageBanner";
 import Nav from "../components/Nav";
 import DropdownInput from "../components/ToAndFrom";
@@ -9,15 +8,23 @@ const Home = () => {
   return (
     <>
       <Nav />
-      <CarouselTop/>
-      <div className=" bg-gray-300 flex justify-center items-center p-10 flex-col sm:flex-row gap-4 w-full">
-        <DropdownInput />
-        To
-        <DropdownInput />
-        {/* <DepartureDate/> */}
+      <CarouselTop />
+
+      {/* Travel Form Section */}
+      <div className="bg-gray-100 py-10 px-4">
+        <div className="max-w-4xl mx-auto bg-white rounded-xl shadow-lg p-6 flex flex-col sm:flex-row sm:items-center gap-6">
+          <div className="w-full sm:w-1/2">
+            <DropdownInput label="From Station" />
+          </div>
+          <div className="text-gray-500 text-center sm:text-xl font-medium">to</div>
+          <div className="w-full sm:w-1/2">
+            <DropdownInput label="To Station" />
+          </div>
+        </div>
       </div>
 
-      <div className="bg-blue-100">
+      {/* Image Banner Section */}
+      <div className="bg-blue-100 py-10 px-4">
         <ImageBanner />
       </div>
     </>
