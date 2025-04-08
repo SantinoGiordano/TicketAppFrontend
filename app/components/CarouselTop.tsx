1d41f170c685a0290e35178d7264420f3f91db06
SantinoGiordano <santinogiordano13@gmail.com>
2025-04-07 20:50:23 -0400

    changed images and slides

--- app/components/CarouselTop.tsx
+++ app/components/CarouselTop.tsx
@@ -1,70 +1,52 @@
+'use client';
+
 import React from "react";
+import Image from "next/image";
 
 const CarouselTop = () => {
   return (
-    <div className="max-w-5xl mx-auto p-5 px-4">
+    <div className="max-w-screen mx-auto px-0">
       <div className="carousel w-full">
         {/* Slide 1 */}
-        <div id="slide1" className="carousel-item relative w-full px-2">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="rounded-box w-full object-cover"
+        <div id="slide1" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/station.jpg"
+            alt="Station"
+            fill
+            className="object-cover w-full h-full "
+            priority
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div>
 
         {/* Slide 2 */}
-        <div id="slide2" className="carousel-item relative w-full px-2">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="rounded-box w-full object-cover"
+        <div id="slide2" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/sideTrainLandscape.jpg"
+            alt="Side of Train"
+            fill
+            className="object-cover w-full h-full "
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div>
 
         {/* Slide 3 */}
-        <div id="slide3" className="carousel-item relative w-full px-2">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="rounded-box w-full object-cover"
+        <div id="slide3" className="carousel-item relative w-full h-[500px]">
+          <Image
+            src="/cityLandscape.jpg"
+            alt="City Landscape"
+            fill
+            className="object-cover w-screen h-full "
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-
-        {/* Slide 4 */}
-        <div id="slide4" className="carousel-item relative w-full px-2">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="rounded-box w-full object-cover"
-          />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
           </div>
         </div>
       </div>
